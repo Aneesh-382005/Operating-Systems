b7e700495a342c876f8cd8c1ed2e1d3a3712388c
Aneesh-382005 <aneesh382005@gmail.com>
2024-10-30 13:17:34 +0530

    smth

---
+++ Scheduling/MoreScheduling.java
@@ -0,0 +1,19 @@
+/*
+ * Write a program using C/C++/Java to simulate the priority scheduling (pre-emptive as well as 
+non-preemptive approach) and RR, CPU scheduling algorithms. The scenario is: user may 
+input n processes with respective CPU burst time and arrival time (also take the priority number 
+in case of priority scheduling). System will ask the user to select the type of algorithm from the 
+list mentioned above. System should display the waiting time for each process, average waiting 
+time for the whole system, and final execution sequence.
+ */
+import java.util.Scanner;
+public class MoreScheduling
+{
+    public static void main(String[]args)
+    {
+        Scanner in = new Scanner(System.in);
+        ProcessList Processes = new ProcessList();
+        
+        
+    }
+}
